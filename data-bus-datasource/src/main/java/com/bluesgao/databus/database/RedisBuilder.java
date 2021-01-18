baafc8cbb0f7f9128e8675b3f323888c0731e972
xin.gao <bluesgao@126.com>
2021-01-18 16:05:01 +0800

    datasource

---
+++ data-bus-datasource/src/main/java/com/bluesgao/databus/database/RedisBuilder.java
@@ -0,0 +1,18 @@
+package com.bluesgao.databus.database;
+
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+@Slf4j
+public class RedisBuilder {
+    public static JedisPool build(RedisProps props) {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxIdle(5);
+        poolConfig.setMinIdle(2);
+        poolConfig.setMaxTotal(10);
+        return new JedisPool(poolConfig, props.getHost(),
+                Integer.parseInt(props.getHost()));
+    }
+}
