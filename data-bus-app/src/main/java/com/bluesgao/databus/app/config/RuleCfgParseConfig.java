7ddafcbac84dc42c7d1474ed152b100d31c6fd21
xin.gao <bluesgao@126.com>
2021-01-19 16:00:08 +0800

    rule parse

--- data-bus-app/src/main/java/com/bluesgao/databus/app/config/RuleCfgParseConfig.java
+++ data-bus-app/src/main/java/com/bluesgao/databus/app/config/RuleCfgParseConfig.java
@@ -1,24 +1,22 @@
 package com.bluesgao.databus.app.config;
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
-import com.bluesgao.databus.app.common.Constants;
 import com.bluesgao.databus.core.rule.RuleCfgHolder;
 import com.bluesgao.databus.core.rule.RuleCfgValidor;
 import com.bluesgao.databus.core.rule.entity.RuleCfg;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.CollectionUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.List;
 
 /**
  * 加载规则配置文件
- * 在DataSource之后加载，验证规则中是否有未配置的DataSource*
  */
 @Slf4j
 @Configuration
@@ -32,7 +30,7 @@ public class RuleCfgParseConfig {
         //todo 文件读取问题
         //String path = new ClassPathResource(rulePath).getPath();
         log.info("*** 开始-加载规则配置文件 *** path:{}", rulePath);
-        List<String> ruleList = FileUtil.listFileNames(rulePath);
+/*        List<String> ruleList = FileUtil.listFileNames(rulePath);
         if (CollectionUtils.isEmpty(ruleList)) {
             log.warn("{}, 未发现配置文件", rulePath);
         }
@@ -49,6 +47,37 @@ public class RuleCfgParseConfig {
                     RuleCfgHolder.putRuleCfg(ruleCfg);
                 }
             }
-        });
+        });*/
+
+        //文件读取
+        String ruleStr = "";
+        try {
+            Resource resource = new ClassPathResource(rulePath);
+            InputStreamReader intput = new InputStreamReader(resource.getInputStream());
+            BufferedReader reader = new BufferedReader(intput);
+            ruleStr = reader.readLine();
+        } catch (Exception e) {
+            log.error("rule-cfg文件读取失败", e.getMessage());
+            throw new RuntimeException("rule-cfg文件读取失败", e);
+        }
+
+        //文件解析
+        List<RuleCfg> ruleCfgList = null;
+        try {
+            ruleCfgList = JSON.parseArray(ruleStr, RuleCfg.class);
+        } catch (Exception e) {
+            log.error("rule-cfg文件解析失败", e.getMessage());
+            throw new RuntimeException("rule-cfg文件解析失败", e);
+
+        }
+
+        for (RuleCfg ruleCfg : ruleCfgList) {
+            // 验证配置是否合法
+            RuleCfgValidor.isValid(ruleCfg);
+            RuleCfgHolder.putRuleCfg(ruleCfg);
+        }
+
+        log.info("*** 结束-加载规则配置文件 *** ruleCfgList:{}", JSON.toJSONString(ruleCfgList));
+
     }
 }
