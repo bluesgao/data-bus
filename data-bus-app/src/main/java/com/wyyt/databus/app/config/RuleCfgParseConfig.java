69c844cf62a662887ca27007afa68618a820bf14
xin.gao <bluesgao@126.com>
2021-03-05 10:50:07 +0800

    cfgfile split

--- data-bus-app/src/main/java/com/wyyt/databus/app/config/RuleCfgParseConfig.java
+++ data-bus-app/src/main/java/com/wyyt/databus/app/config/RuleCfgParseConfig.java
@@ -21,14 +21,15 @@ import java.util.List;
 @Configuration
 public class RuleCfgParseConfig {
 
-    @Value("${databus.rule.path}")
-    protected String rulePath;
+    //@Value("${databus.rule.path}")
+    //protected String rulePath;
+    @Value("#{'${databus.rule.files}'.split(',')}")
+    private String[] ruleFileList;
 
     @PostConstruct
     public void init() {
         //todo 文件读取问题
         //String path = new ClassPathResource(rulePath).getPath();
-        log.info("*** 开始-加载规则配置文件 *** path:{}", rulePath);
 /*        List<String> ruleList = FileUtil.listFileNames(rulePath);
         if (CollectionUtils.isEmpty(ruleList)) {
             log.warn("{}, 未发现配置文件", rulePath);
@@ -85,39 +86,47 @@ public class RuleCfgParseConfig {
             }
         }*/
 
+        log.info("*** 开始处理规则配置文件 *** ruleFileList:{}", JSON.toJSONString(ruleFileList));
+        for (String ruleFileName : ruleFileList) {
+            log.info("*** 开始解析规则配置文件-{}", ruleFileName);
+            //获取文件内容
+            String ruleStr = getFileContent(ruleFileName);
+            //文件解析
+            List<RuleCfg> ruleCfgList = null;
+            try {
+                ruleCfgList = JSON.parseArray(ruleStr, RuleCfg.class);
+            } catch (Exception e) {
+                log.error("规则配置文件{}解析失败", ruleFileName, e.getMessage());
+                throw new RuntimeException("规则配置文件解析失败", e);
+            }
+            log.debug("rule-cfg文件解析:{}", JSON.toJSONString(ruleCfgList));
 
-        Resource resource = new ClassPathResource(rulePath);
+            for (RuleCfg ruleCfg : ruleCfgList) {
+                // 验证配置是否合法
+                //RuleCfgValidor.isValid(ruleCfg);
+                RuleCfgHolder.putRuleCfg(ruleCfg);
+            }
+            log.info("*** 完成解析规则配置文件-{},规则数量{}", ruleFileName, ruleCfgList.size());
+        }
+        log.info("*** 结束处理规则配置文件 ***");
+    }
 
-        String ruleStr = null;
+    private String getFileContent(String fileName) {
+        Resource resource = new ClassPathResource(fileName);
+
+        String content = null;
         try {
             byte[] bytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
-            ruleStr = new String(bytes, StandardCharsets.UTF_8);
+            content = new String(bytes, StandardCharsets.UTF_8);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        log.info("ruleStr:{}", ruleStr);
+        log.debug("content:{}", content);
 
-        if (ruleStr == null || ruleStr.length() <= 0) {
-            throw new RuntimeException("rule-cfg文件读取失败");
+        if (content == null || content.length() <= 0) {
+            throw new RuntimeException("文件读取失败:" + fileName);
         }
-        //文件解析
-        List<RuleCfg> ruleCfgList = null;
-        try {
-            ruleCfgList = JSON.parseArray(ruleStr, RuleCfg.class);
-        } catch (Exception e) {
-            log.error("rule-cfg文件解析失败", e.getMessage());
-            throw new RuntimeException("rule-cfg文件解析失败", e);
-
-        }
-        log.info("rule-cfg文件解析:{}", JSON.toJSONString(ruleCfgList));
-
-        for (RuleCfg ruleCfg : ruleCfgList) {
-            // 验证配置是否合法
-            //RuleCfgValidor.isValid(ruleCfg);
-            RuleCfgHolder.putRuleCfg(ruleCfg);
-        }
-
-        log.info("*** 结束-加载规则配置文件 *** ruleCfgList:{}", JSON.toJSONString(ruleCfgList));
-
+        return content;
     }
+
 }
